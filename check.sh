f37869e6a67df8889cf6d8f555a15664585f5763
zixingdeng <zixingdeng@tencent.com>
2024-11-19 15:52:37 +0800

    支持上报junit xml标准格式用例结果文件

--- check.sh
+++ check.sh
@@ -5,8 +5,6 @@ set -exu -o pipefail
 if [[ -z "${GITHUB_ACTIONS+x}" ]]; then
   echo "GITHUB_ACTIONS environment variable is not set.Use local mode."
   uv lock
-  uv run ruff format src
-  uv run ruff format tests
   uv run pytest tests --durations=5 --cov=. --cov-fail-under=90 --cov-report term
 else
   echo "GITHUB_ACTIONS environment variable is set.Use CI mode."
